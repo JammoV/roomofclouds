e56125b6c4ed4773eef731a69f0cb19baa849985
Jarmo Vleeshouwer <jarmovleeshouwer@gmail.com>
2022-03-06 21:09:39 +0100

    Add header work break

--- web/styles/theme.ts
+++ web/styles/theme.ts
@@ -3,13 +3,18 @@ import { createTheme } from '@mui/material/styles'
 export default createTheme({
     spacing: 8,
     typography: {
+        h1: {
+            wordBreak: 'break-word',
+        },
         h2: {
             fontSize: '2.5rem',
             fontFamily: ['Yuji Syuku', 'serif'].join(','),
+            wordBreak: 'break-word',
         },
         h3: {
             fontSize: '2.1rem',
             fontFamily: ['Roboto', 'serif'].join(','),
+            wordBreak: 'break-word',
         },
     },
 })
